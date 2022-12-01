e9c5358d8fb82a2425fd79621c7451f3b459ed4e
Chanyang Park <mn99108@naver.com>
2022-12-01 12:58:22 +0900

    내비게이션 바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -8,6 +8,26 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
+
+
+
     def test_post_list(self):
 
         #  1.1. 포스트 목록 페이지를 가져온다
@@ -17,11 +37,8 @@ class TestView(TestCase):
         # 페이지 타이틀은 'Blog'이다
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        # 1.4. 내비게이션 바가 있다.
-        navbar = soup.nav
-        # 1.5. Blog,About Me 라는 문구가 내비게이션 바에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+
+        self.navbar_test(soup)
 
         #  2.1. 포스트(게시물)가 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -74,10 +91,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # 2.2. 포스트 목록 페이지와 똑같은 내비게이션 바가 있다
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.3. 첫 번째 포스트의 제목이 포스트 영역(post_area.text)에 있다.
         main_area = soup.find('div', id='main-area')
